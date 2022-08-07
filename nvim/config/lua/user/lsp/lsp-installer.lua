a10cb6f94b80158d027af5cb8ae47dcd0052c238
Eik Emil Bengtsson <me@eikc.dk>
2022-08-07 17:11:01 +0200

    feat: rust analyzer and gh-clone
    Signed-off-by: Eik Emil Bengtsson <me@eikc.dk>

--- nvim/config/lua/user/lsp/lsp-installer.lua
+++ nvim/config/lua/user/lsp/lsp-installer.lua
@@ -27,6 +27,11 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
 	end
 
+	if server.name == "rust_analyzer" then
+		local rust_opts = require("user.lsp.settings.rust_analyzer")
+		opts = vim.tbl_deep_extend("force", rust_opts, opts)
+	end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
---
+++ nvim/config/lua/user/lsp/settings/rust_analyzer.lua
@@ -0,0 +1,18 @@
+return {
+	settings = {
+		imports = {
+			granularity = {
+				group = "module",
+			},
+			prefix = "self",
+		},
+		cargo = {
+			buildScripts = {
+				enable = true,
+			},
+		},
+		procMacro = {
+			enable = true,
+		},
+	},
+}
