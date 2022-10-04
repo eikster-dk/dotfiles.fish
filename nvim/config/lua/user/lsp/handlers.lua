862e677cb9077f2e53d9fcca4bf1abd94d6a96db
Eik Emil Bengtsson <me@eikc.dk>
2022-10-04 13:30:52 +0200

    fix(nvim): updated documentFormattingProvider
    Signed-off-by: Eik Emil Bengtsson <me@eikc.dk>

--- nvim/config/lua/user/lsp/handlers.lua
+++ nvim/config/lua/user/lsp/handlers.lua
@@ -105,9 +105,9 @@ M.on_attach = function(client, bufnr)
 	end
 
 	if client.name == "sumneko_lua" then
-		client.server_capabilities.document_formatting = false -- 0.7 and earlier
-		client.server_capabilities.document_formatting = false -- 0.7 and earlier
-		-- client.server_capabilities.documentFormattingProvider = false -- 0.8 and later
+		-- client.server_capabilities.document_formatting = false -- 0.7 and earlier
+		-- client.server_capabilities.document_formatting = false -- 0.7 and earlier
+		client.server_capabilities.documentFormattingProvider = false -- 0.8 and later
 	end
 
 	lsp_keymaps(bufnr)
