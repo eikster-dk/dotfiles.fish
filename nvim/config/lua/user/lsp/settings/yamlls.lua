a30a4f2dd454f989566574d26b31a1e30847de25
Eik Emil Bengtsson <me@eikc.dk>
2022-10-19 12:41:13 +0200

    feat(yamlls): added kustomization to yamlls config
    Signed-off-by: Eik Emil Bengtsson <me@eikc.dk>

--- nvim/config/lua/user/lsp/settings/yamlls.lua
+++ nvim/config/lua/user/lsp/settings/yamlls.lua
@@ -1,6 +1,7 @@
 local schema = {}
 schema["kubernetes"] = { "/kustomize/**/*.yaml", "/kustomize/**/*.yml" }
 schema["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+schema["https://json.schemastore.org/kustomization.json"] = { "/kustomize/**/kustomization.yaml", "/kustomize/**/kustomization.yml"}
 
 return {
 	settings = {
